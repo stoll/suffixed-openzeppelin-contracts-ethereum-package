a986556de4b7a995336d1b9cd9c94c771440eb05
Sebastian Stoll <stoll@users.noreply.github.com>
2020-10-16 13:24:54 +0200

    Add SPDX License Identifier
    To avoid compilers from giving warnings.

--- contracts/mocks/ERC1820ImplementerMock.sol
+++ contracts/mocks/ERC1820ImplementerMock.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
 import "../introspection/ERC1820Implementer.sol";
--- contracts/mocks/ERC20SnapshotMock.sol
+++ contracts/mocks/ERC20SnapshotMock.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
 import "../token/ERC20/ERC20Snapshot.sol";
